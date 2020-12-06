d390f3d969e59ba018beabd3f1f029f751e36baf
Piotr <piotr.gleska@openanalytics.eu>
2020-12-06 21:51:05 +0100

    add schemas

---
+++ jira-put2.0.1.sql
@@ -0,0 +1,390 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5ubuntu0.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 06, 2020 at 09:43 PM
+-- Server version: 5.7.32-0ubuntu0.18.04.1
+-- PHP Version: 7.2.24-0ubuntu0.18.04.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jira-put`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`dba`@`localhost` PROCEDURE `zmien_nazwe_zespolu` (IN `stary` VARCHAR(63), IN `nowy` VARCHAR(63))  BEGIN
+	INSERT INTO `zespol`(`zespol`.`nazwa`, `zespol`.`liczba_czlonkow`) VALUES (nowy, 0);
+    UPDATE `pracownik`SET `pracownik`.`zespol`=nowy WHERE `pracownik`.`zespol`=stary;
+    DELETE FROM `zespol` WHERE `zespol`.`nazwa`=stary;
+END$$
+
+--
+-- Functions
+--
+CREATE DEFINER=`dba`@`localhost` FUNCTION `roznica_czasow` (`id` INT) RETURNS INT(11) BEGIN
+	DECLARE szacunkowy timestamp;
+    DECLARE rzeczywisty timestamp;
+	SELECT issue.szacunkowy_czas_trwania, issue.rzeczywisty_czas_trwania INTO szacunkowy, rzeczywisty FROM issue WHERE issue.identyfikator = id;
+	RETURN TIMESTAMPDIFF(SECOND, szacunkowy, rzeczywisty);
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `epic`
+--
+
+CREATE TABLE `epic` (
+  `termin_realizacji` datetime DEFAULT NULL,
+  `projekt` varchar(63) NOT NULL,
+  `issue_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `firma_zewnetrzna`
+--
+
+CREATE TABLE `firma_zewnetrzna` (
+  `nip` int(11) NOT NULL,
+  `nazwa` varchar(255) NOT NULL,
+  `adres` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issue`
+--
+
+CREATE TABLE `issue` (
+  `identyfikator` int(11) NOT NULL,
+  `nazwa` varchar(63) NOT NULL,
+  `opis` text,
+  `szacunkowy_czas_trwania` timestamp NULL DEFAULT NULL,
+  `rzeczywisty_czas_trwania` timestamp NULL DEFAULT NULL,
+  `podtyp` enum('epic','story','task') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kontrakt`
+--
+
+CREATE TABLE `kontrakt` (
+  `numer_umowy` varchar(31) NOT NULL,
+  `kwota` float NOT NULL,
+  `opis_warunkow` text,
+  `firma_zew` int(11) NOT NULL,
+  `projekt` varchar(63) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pracownik`
+--
+
+CREATE TABLE `pracownik` (
+  `login` varchar(31) NOT NULL,
+  `haslo` varchar(255) NOT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  `imie` varchar(31) NOT NULL,
+  `nazwisko` varchar(31) NOT NULL,
+  `pensja` float NOT NULL,
+  `stanowisko` varchar(31) NOT NULL,
+  `zespol` varchar(63) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Triggers `pracownik`
+--
+DELIMITER $$
+CREATE TRIGGER `after_delete_pracownik` AFTER DELETE ON `pracownik` FOR EACH ROW UPDATE zespol SET liczba_czlonkow=liczba_czlonkow-1 WHERE nazwa=OLD.zespol
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `after_insert_pracownik` AFTER INSERT ON `pracownik` FOR EACH ROW UPDATE zespol SET liczba_czlonkow=liczba_czlonkow+1 WHERE nazwa=NEW.zespol
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `after_update_pracownik` AFTER UPDATE ON `pracownik` FOR EACH ROW BEGIN
+    UPDATE zespol SET liczba_czlonkow=liczba_czlonkow+1 WHERE nazwa=NEW.zespol;
+    UPDATE zespol SET liczba_czlonkow=liczba_czlonkow-1 WHERE nazwa=OLD.zespol;
+  END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projekt`
+--
+
+CREATE TABLE `projekt` (
+  `nazwa` varchar(63) NOT NULL,
+  `wersja` varchar(7) NOT NULL,
+  `opis` text CHARACTER SET latin1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stanowisko`
+--
+
+CREATE TABLE `stanowisko` (
+  `nazwa` varchar(31) NOT NULL,
+  `pensja_minimalna` int(11) NOT NULL,
+  `pensja_maksymalna` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `story`
+--
+
+CREATE TABLE `story` (
+  `issue_id` int(11) NOT NULL,
+  `zespol` varchar(63) NOT NULL,
+  `epic_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE `task` (
+  `typ` tinyint(4) NOT NULL,
+  `issue_id` int(11) NOT NULL,
+  `story_id` int(11) NOT NULL,
+  `prac_login` varchar(31) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `technologia`
+--
+
+CREATE TABLE `technologia` (
+  `nazwa` varchar(63) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tech_prac`
+--
+
+CREATE TABLE `tech_prac` (
+  `tech_nazwa` varchar(63) NOT NULL,
+  `prac_login` varchar(31) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tech_proj`
+--
+
+CREATE TABLE `tech_proj` (
+  `tech_nazwa` varchar(63) NOT NULL,
+  `proj_nazwa` varchar(63) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zespol`
+--
+
+CREATE TABLE `zespol` (
+  `nazwa` varchar(63) NOT NULL,
+  `liczba_czlonkow` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `epic`
+--
+ALTER TABLE `epic`
+  ADD KEY `epic_proj_fkey` (`projekt`),
+  ADD KEY `epic_issue_fkey` (`issue_id`);
+
+--
+-- Indexes for table `firma_zewnetrzna`
+--
+ALTER TABLE `firma_zewnetrzna`
+  ADD PRIMARY KEY (`nip`),
+  ADD UNIQUE KEY `nazwa_firma_key` (`nazwa`);
+
+--
+-- Indexes for table `issue`
+--
+ALTER TABLE `issue`
+  ADD PRIMARY KEY (`identyfikator`),
+  ADD KEY `identyfikator` (`identyfikator`),
+  ADD KEY `identyfikator_2` (`identyfikator`);
+
+--
+-- Indexes for table `kontrakt`
+--
+ALTER TABLE `kontrakt`
+  ADD PRIMARY KEY (`numer_umowy`),
+  ADD KEY `kontr_proj_fkey` (`projekt`),
+  ADD KEY `kontr_firm_zew_fkey` (`firma_zew`);
+
+--
+-- Indexes for table `pracownik`
+--
+ALTER TABLE `pracownik`
+  ADD PRIMARY KEY (`login`),
+  ADD KEY `prac_zespol_fkey` (`zespol`),
+  ADD KEY `prac_stanowisko_fkey` (`stanowisko`),
+  ADD KEY `nazwisko` (`nazwisko`);
+
+--
+-- Indexes for table `projekt`
+--
+ALTER TABLE `projekt`
+  ADD PRIMARY KEY (`nazwa`),
+  ADD KEY `nazwa` (`nazwa`);
+
+--
+-- Indexes for table `stanowisko`
+--
+ALTER TABLE `stanowisko`
+  ADD PRIMARY KEY (`nazwa`);
+
+--
+-- Indexes for table `story`
+--
+ALTER TABLE `story`
+  ADD KEY `story_epic_fkey` (`epic_id`),
+  ADD KEY `story_issue_fkey` (`issue_id`),
+  ADD KEY `story_zespol_fkey` (`zespol`);
+
+--
+-- Indexes for table `task`
+--
+ALTER TABLE `task`
+  ADD KEY `task_story_fkey` (`story_id`),
+  ADD KEY `task_issue_fkey` (`issue_id`),
+  ADD KEY `prac_login` (`prac_login`);
+
+--
+-- Indexes for table `technologia`
+--
+ALTER TABLE `technologia`
+  ADD PRIMARY KEY (`nazwa`);
+
+--
+-- Indexes for table `tech_prac`
+--
+ALTER TABLE `tech_prac`
+  ADD KEY `tech_prac_technologia_fkey` (`tech_nazwa`),
+  ADD KEY `prac_login` (`prac_login`);
+
+--
+-- Indexes for table `tech_proj`
+--
+ALTER TABLE `tech_proj`
+  ADD KEY `tech_proj_projekt_fkey` (`proj_nazwa`),
+  ADD KEY `tech_proj_technologia_fkey` (`tech_nazwa`);
+
+--
+-- Indexes for table `zespol`
+--
+ALTER TABLE `zespol`
+  ADD PRIMARY KEY (`nazwa`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `issue`
+--
+ALTER TABLE `issue`
+  MODIFY `identyfikator` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `epic`
+--
+ALTER TABLE `epic`
+  ADD CONSTRAINT `epic_issue_fkey` FOREIGN KEY (`issue_id`) REFERENCES `issue` (`identyfikator`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `epic_proj_fkey` FOREIGN KEY (`projekt`) REFERENCES `projekt` (`nazwa`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `kontrakt`
+--
+ALTER TABLE `kontrakt`
+  ADD CONSTRAINT `kontr_firm_zew_fkey` FOREIGN KEY (`firma_zew`) REFERENCES `firma_zewnetrzna` (`nip`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `kontr_proj_fkey` FOREIGN KEY (`projekt`) REFERENCES `projekt` (`nazwa`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `pracownik`
+--
+ALTER TABLE `pracownik`
+  ADD CONSTRAINT `prac_stanowisko_fkey` FOREIGN KEY (`stanowisko`) REFERENCES `stanowisko` (`nazwa`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `prac_zespol_fkey` FOREIGN KEY (`zespol`) REFERENCES `zespol` (`nazwa`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `story`
+--
+ALTER TABLE `story`
+  ADD CONSTRAINT `story_epic_fkey` FOREIGN KEY (`epic_id`) REFERENCES `epic` (`issue_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `story_issue_fkey` FOREIGN KEY (`issue_id`) REFERENCES `issue` (`identyfikator`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `story_zespol_fkey` FOREIGN KEY (`zespol`) REFERENCES `zespol` (`nazwa`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `task`
+--
+ALTER TABLE `task`
+  ADD CONSTRAINT `task_issue_fkey` FOREIGN KEY (`issue_id`) REFERENCES `issue` (`identyfikator`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `task_pracownik_fkey` FOREIGN KEY (`prac_login`) REFERENCES `pracownik` (`login`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `task_story_fkey` FOREIGN KEY (`story_id`) REFERENCES `story` (`issue_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `tech_prac`
+--
+ALTER TABLE `tech_prac`
+  ADD CONSTRAINT `tech_prac_pracownik_fkey` FOREIGN KEY (`prac_login`) REFERENCES `pracownik` (`login`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `tech_prac_technologia_fkey` FOREIGN KEY (`tech_nazwa`) REFERENCES `technologia` (`nazwa`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `tech_proj`
+--
+ALTER TABLE `tech_proj`
+  ADD CONSTRAINT `tech_proj_projekt_fkey` FOREIGN KEY (`proj_nazwa`) REFERENCES `projekt` (`nazwa`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `tech_proj_technologia_fkey` FOREIGN KEY (`tech_nazwa`) REFERENCES `technologia` (`nazwa`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
